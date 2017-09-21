b6bdc1482c07569601e3446c30365baa735c6111
prashasthip <prashasthip@gmail.com>
2017-09-21 18:52:05 +0000

    Merged last sem UDF code till planner

--- src/type/types.cpp
+++ src/type/types.cpp
@@ -468,6 +468,9 @@ std::string StatementTypeToString(StatementType type) {
     case StatementType::UPDATE: {
       return "UPDATE";
     }
+    case StatementType::CREATE_FUNC: {
+      return "CREATE_FUNC";
+    }
     case StatementType::DELETE: {
       return "DELETE";
     }
@@ -521,6 +524,8 @@ StatementType StringToStatementType(const std::string& str) {
     return StatementType::DELETE;
   } else if (upper_str == "CREATE") {
     return StatementType::CREATE;
+  } else if (upper_str == "CREATE_FUNC") {
+    return StatementType::CREATE_FUNC;
   } else if (upper_str == "DROP") {
     return StatementType::DROP;
   } else if (upper_str == "PREPARE") {
